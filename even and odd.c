ec21a5ff9cea12ff701cfec5ac6b53e892ba3d3a
Ivy Grace M. Mapalad <73746413+IvyGraceMapalad@users.noreply.github.com>
2020-11-06 22:24:28 +0800

    Add files via upload

---
+++ even and odd.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+int Num1;
+printf("Enter a number: ");
+scanf("%d", &Num1);
+if((Num1%2)==0)
+{
+    printf("The number is Even.");
+}
+else
+{
+    printf("The number is Odd.");
+}
+getch();
+}
+
+
+
